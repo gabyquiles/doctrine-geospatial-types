f706796aac4e46a65aa1d96385f8ef75fbe6d9c9
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2019-09-02 17:12:30 -0400

    - Add Distance DQL

---
+++ src/DQL/PointStr.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GabyQuiles\Doctrine\GeoSpatial\DQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * POINT_STR function for querying using Point objects as parameters
+ *
+ * Usage: POINT_STR(:param) where param should be mapped to $point where $point is Wantlet\ORM\Point
+ *        without any special typing provided (eg. so that it gets converted to string)
+ */
+class PointStr extends FunctionNode
+{
+    private $arg;
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'PointFromText(' . $this->arg->dispatch($sqlWalker) . ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->arg = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+}
--- src/Type/PointType.php
+++ src/Type/PointType.php
@@ -7,31 +7,27 @@ use Doctrine\DBAL\Types\Type;
 
 class PointType extends Type
 {
-    const POINT = "point";
-
-    /**
-     * Gets the SQL declaration snippet for a field of this type.
-     *
-     * @param mixed[] $fieldDeclaration The field declaration.
-     * @param AbstractPlatform $platform The currently used database platform.
-     *
-     * @return string
-     */
-    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
-    {
-        return 'POINT';
-    }
+    const POINT = 'point';
 
     /**
      * Gets the name of this type.
      *
      * @return string
-     *
-     * @todo Needed?
      */
     public function getName()
     {
         return self::POINT;
+    }
+
+    /**
+     * Gets the SQL declaration snippet for a field of this type.
+     *
+     * @param array $fieldDeclaration The field declaration.
+     * @param AbstractPlatform $platform The currently used database platform.
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'POINT';
     }
 
     public function convertToPHPValue($value, AbstractPlatform $platform)
@@ -44,7 +40,7 @@ class PointType extends Type
         $data = unpack('x/x/x/x/corder/Ltype/dlat/dlon', $value);
         return new Point($data['lat'], $data['lon']);
     }
-    
+
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
         if (!$value) return;
